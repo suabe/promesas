4864c8a5e1c28da836594b2cd151eca0032178d5
Ernesto <ernesto.suarez@erasocial.com>
2014-08-15 08:48:15 -0500

    Archivos CI
    Eliminando archivos CI para evaluación del codigo

--- application/libraries/excel.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once APPPATH."/third_party/PHPExcel.php"; 
-
-	class Excel extends PHPExcel { 
-	    public function __construct() { 
-	        parent::__construct(); 
-	    } 
-	}
-?>
